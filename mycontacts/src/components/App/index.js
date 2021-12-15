d8d26efd08f2c849b2840e0291557b749ab41f4b
William <williambronstrup13@gmail.com>
2021-12-14 23:40:32 -0300

    Configurando Rotas

--- mycontacts/src/components/App/index.js
+++ mycontacts/src/components/App/index.js
@@ -1,24 +1,25 @@
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
 
 import Header from '../Header';
-import ContactsList from '../ContactsList';
+import Routes from '../../Routes';
 
 import { Container } from './styles';
 
-function App() {
+export default function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles />
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
 
-      <Container>
-        <Header />
-        <ContactsList />
-      </Container>
-    </ThemeProvider>
+        <Container>
+          <Header />
+          <Routes />
+        </Container>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
-
-export default App;
---
+++ mycontacts/src/pages/Home/index.js
@@ -0,0 +1,5 @@
+import ContactsList from '../../components/ContactsList';
+
+export default function Home() {
+  return <ContactsList />;
+}
