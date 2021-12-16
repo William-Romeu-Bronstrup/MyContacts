c88df7e7c8c3095a1bafc5fb12e2eb424c980f1f
William <williambronstrup13@gmail.com>
2021-12-16 00:24:14 -0300

    Criando o componente Button

---
+++ mycontacts/src/components/Button.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export default styled.button`
+  width: 100%;
+  height: 52px;
+  border: none;
+  background: ${({ theme }) => theme.colors.primary.main};
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  font-weight: bold;
+  color: #FFF;
+  border-radius: 4px;
+  transition: background .2s ease-in;
+
+  &:hover {
+    background: ${({ theme }) => theme.colors.primary.light};
+  }
+
+  &:active {
+    background: ${({ theme }) => theme.colors.primary.dark};
+  }
+
+  &[disabled] {
+    background: #CCC;
+    cursor: default;
+  }
+`;
