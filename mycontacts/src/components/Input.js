c32868976fe11a42312bc1c79e25e3cc84d5e932
William <williambronstrup13@gmail.com>
2021-12-16 00:14:37 -0300

    Criando os componentes Input e Select

---
+++ mycontacts/src/components/Input.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export default styled.input`
+  width: 100%;
+  border: none;
+  outline: none;
+  border: 2px solid #FFF;
+  background: #FFF;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  height: 52px;
+  border-radius: 4px;
+  padding: 0 16px;
+  font-size: 16px;
+  transition: border-color .2s ease-in;
+
+  &:focus {
+    border-color: ${({ theme }) => theme.colors.primary.main};
+  }
+`;
--- mycontacts/src/pages/NewContact/index.js
+++ mycontacts/src/pages/NewContact/index.js
@@ -1,7 +1,20 @@
 import PageHeader from '../../components/PageHeader';
 
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+
 export default function NewContact() {
   return (
-    <PageHeader title="Novo Contato" />
+    <>
+      <PageHeader title="Novo Contato" />
+
+      <Input input="text" placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
+    </>
   );
 }
