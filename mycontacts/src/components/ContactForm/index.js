a6d4c6f09ca4157e3a20daa4946fde0597fe5972
William <williambronstrup13@gmail.com>
2021-12-30 16:50:56 -0300

    Loader e eventos no form

--- mycontacts/src/components/ContactForm/index.js
+++ mycontacts/src/components/ContactForm/index.js
@@ -9,14 +9,22 @@ import Select from '../Select';
 import Button from '../Button';
 
 export default function ContactForm({ buttonLabel }) {
+  // input.addEventListener('change', function (event) {
+  // });
+
   return (
     <Form>
       <FormGroup>
-        <Input placeholder="Nome" />
+        <Input
+          placeholder="Nome"
+          onChange={(event) => console.log(event.target.value)}
+        />
       </FormGroup>
 
-      <FormGroup>
-        <Input placeholder="E-mail" />
+      <FormGroup
+        error="O formato do e-mail é inválido."
+      >
+        <Input placeholder="E-mail" error />
       </FormGroup>
 
       <FormGroup>
