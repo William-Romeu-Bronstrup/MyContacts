482d38a8b3ee00b419e02daca3e8c65490f7bf6b
William <williambronstrup13@gmail.com>
2021-12-30 16:05:13 -0300

    React Portals, modal

--- mycontacts/src/components/Button.js
+++ mycontacts/src/components/Button.js
@@ -1,7 +1,7 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export default styled.button`
-  width: 100%;
+  padding: 0 16px;
   height: 52px;
   border: none;
   background: ${({ theme }) => theme.colors.primary.main};
@@ -23,4 +23,16 @@ export default styled.button`
     background: #CCC;
     cursor: default;
   }
+
+  ${({ theme, danger }) => danger && css`
+    background: ${theme.colors.danger.main};
+
+    &:hover {
+    background: ${theme.colors.danger.light};
+    }
+
+    &:active {
+      background: ${theme.colors.danger.dark};
+    }
+  `}
 `;
