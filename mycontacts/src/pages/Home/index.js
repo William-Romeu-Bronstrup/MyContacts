b863ebc7d229d76990d9e4f362af9d80f2e11a3d
William <williambronstrup13@gmail.com>
2022-01-24 16:39:18 -0300

    Melhorando a lógica de requisições

--- mycontacts/src/pages/Home/index.js
+++ mycontacts/src/pages/Home/index.js
@@ -10,7 +10,7 @@ import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 
 import Loader from '../../components/Loader';
-import delay from '../../utils/delay';
+import ContactsService from '../../services/ContactsService';
 
 export default function Home() {
   const [contacts, setContacts] = useState([]);
@@ -23,21 +23,21 @@ export default function Home() {
   )), [contacts, searchTerm]);
 
   useEffect(() => {
-    setIsLoading(true);
+    async function loadContacts() {
+      try {
+        setIsLoading(true);
 
-    fetch(`http://localhost:3001/contactss?orderBy=${orderBy}`)
-      .then(async (response) => {
-        await delay(2000);
+        const contactsList = await ContactsService.listContacts(orderBy);
 
-        const json = await response.json();
-        setContacts(json);
-      })
-      .catch((error) => {
-        console.log('erro:', error);
-      })
-      .finally(() => {
+        setContacts(contactsList);
+      } catch (error) {
+        console.log('error', error);
+      } finally {
         setIsLoading(false);
-      });
+      }
+    }
+
+    loadContacts();
   }, [orderBy]);
 
   function handleToggleOrderBy() {
